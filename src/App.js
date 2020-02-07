54e70971475d05c4494a339f99c753b79b275669
victorflorindo9@gmail.com <19180130@CFP127ALUNO.LOCAL>
2020-02-07 14:37:07 -0300

    aula de react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      lista: []
+    }
+
+  }
+
+  async componentDidMount() {
+    let resposta = await fetch('http://localhost:3000/series')
+    const series = await resposta.json()
+    this.setState({lista: series})
+  }
+
+
+  render() {
+    console.log("estou sendo segurado na tela")
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Cadastro de Series
+        <div className="form">
+            <form method="post">
+                <div className="form">
+                    <label htmlFor="nome">Nome</label>
+                    <input type="text" id="nome" name="nome"/>
+                    <label htmlFor="lancamento">Ano de lançamento</label>
+                    <input type="text" id="lancamento" name="lancamento"/>
+                    <label htmlFor="temporadas">Temporadas</label>
+                    <input type="text" id="temporadas" name="temporadas"/>
+                    <label htmlFor="sinopse">Sinopse</label>
+                    <textarea id="sinopse" name="sinopse"></textarea>
+                    <button type="submit">Salvar</button>
+                </div>
+              </form>
+                <div className="list">
+                    <table>
+                      <thead>
+                        <tr>
+                          <td>Nome</td>
+                          <td>Lançamento</td>
+                          <td>Temporadas</td>
+                          <td>Sinopse</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.state.lista.map(serie => {
+                          return(
+                            <tr key={serie.id}>
+                            <td>{serie.nome}</td>
+                            <td>{serie.lancamento}</td>
+                            <td>{serie.temporada}</td>
+                            <td>{serie.sinopse}</td>
+                          </tr>
+                          )
+                        })}
+                      </tbody>
+                    </table>
+                </div>
+        </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
